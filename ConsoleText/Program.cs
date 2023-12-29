543614fe25da023e4e37efe14f6674ea269612c8
sda0503 <sda0503@naver.com>
2023-12-29 11:47:19 +0900

    프로젝트 생성

---
+++ ConsoleText/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("체중(kg)를 입력하세요: (숫자만)");
+float weight = float.Parse(Console.ReadLine());
+
+Console.WriteLine("키(cm)를 입력하세요: (숫자만)");
+float height = float.Parse(Console.ReadLine());
+float m_height = height / 100;
+
+float bmi = weight / (m_height * m_height);
+Console.WriteLine("당신의 BMI: " + bmi);
