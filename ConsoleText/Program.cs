f54954e4ae5468f3024fc83e5ff1453988217364
sda0503 <sda0503@naver.com>
2023-12-29 21:24:55 +0900

    장비장착 수정

--- ConsoleText/Program.cs
+++ ConsoleText/Program.cs
@@ -1,16 +1,21 @@
-﻿void init()
+﻿using System.Collections.Generic;
+
+void init()
 {
+    //플레이어 초기화
     Player player = new Player();
+    //게임 시작
     Town(player);
 }
 
 
+//마을
 void Town(Player player)
 {
     Console.Clear();
     Console.WriteLine("");
     Console.WriteLine("");
-    Console.WriteLine("1.상태 보기");
+    Console.WriteLine("1.상태보기");
     Console.WriteLine("2.인벤토리");
     Console.WriteLine("3.상점");
     Console.WriteLine("4.여관");
@@ -36,6 +41,7 @@ void Town(Player player)
     }
 }
 
+//상태보기
 void State(Player player)
 {
     
@@ -52,10 +58,7 @@ void State(Player player)
     Console.WriteLine("원하시는 행동을 입력 해주세요.");
     int choice = int.Parse(Console.ReadLine());
 
-    if(choice == 0)
-    {
-        Town(player);
-    }
+    if(choice == 0)Town(player);
     else
     {
         Console.WriteLine("잘못된 입력입니다.");
@@ -64,11 +67,12 @@ void State(Player player)
     }
 }
 
+//인벤토리
 void Inventory(Player player)
 {
     Console.Clear();
     Console.WriteLine("[아이템 목록]");
-    player.GetItem();
+    player.GetItemName();
     Console.WriteLine("");
     Console.WriteLine("1.장착 관리");
     Console.WriteLine("0.나가기");
@@ -95,34 +99,105 @@ void Equipment(Player player)
 {
     Console.Clear();
     Console.WriteLine("[장착 관리]");
-    Console.WriteLine("1.무기 -");
-    Console.WriteLine("2.방어구 -");
-    Console.WriteLine("3.장신구 -");
+    Console.WriteLine("1.무기 -" + player.equipment[0]);
+    Console.WriteLine("2.방어구 -" + player.equipment[1]);
+    Console.WriteLine("3.장신구 -" + player.equipment[2]);
     Console.WriteLine("0.나가기");
     Console.WriteLine("");
     Console.WriteLine("원하시는 행동을 입력 해주세요.");
     int choice = int.Parse(Console.ReadLine());
-    if (choice == 0)
-    {
-        Inventory(player);
-    }
-    else if (choice == 1)
-    {
-        Equipment(player);
-    }
-    else if (choice == 2)
-    {
-        Equipment(player);
-    }
-    else if (choice == 3)
-    {
-        Equipment(player);
-    }
+    if (choice == 0) Inventory(player);
+    else if (choice == 1) EquipmentItem(player,1);
+    else if (choice == 2) EquipmentItem(player,2);
+    else if (choice == 3) EquipmentItem(player,3);
     else
     {
         Console.WriteLine("잘못된 입력입니다.");
         Thread.Sleep(1000);
         Equipment(player);
+    }
+}
+
+//장비 장착
+void EquipmentItem(Player player, int type) 
+{
+    //장비 분류
+    List<Item> weaponItem = new List<Item>(); //무기 리스트
+    List<Item> armorItem = new List<Item>();  //방어구 리스트
+    List<Item> accItem = new List<Item>();    //장신구 리스트
+    for (int i = 0; i < player.list.Count; i++)
+    {
+        //무기 인것만 담자
+        if (player.list[i].type == "[무기]")
+        {
+            weaponItem.Add(player.list[i]);
+        }
+        if (player.list[i].type == "[방어구]")
+        {
+            armorItem.Add(player.list[i]);
+        }
+        if (player.list[i].type == "[장신구]")
+        {
+            accItem.Add(player.list[i]);
+        }
+    }
+
+    if (type == 1)
+    {
+        Console.Clear();
+        Console.WriteLine("[무기 장착]");
+        Console.WriteLine("");
+        Console.WriteLine("장착 가능무기");
+        for (int i = 0; i < weaponItem.Count; i++)
+        {
+            Console.WriteLine((i+1)+ weaponItem[i].name);
+        }
+        Console.WriteLine("0.나가기");
+        Console.WriteLine("");
+        Console.WriteLine("장착을 원하는 무기의 번호를 고르세요");
+        Console.WriteLine("");
+        Console.WriteLine("원하시는 행동을 입력 해주세요.");
+        Console.WriteLine("0 ~ "+ weaponItem.Count);
+        int choice = int.Parse(Console.ReadLine());
+        if (choice == 0) Inventory(player);
+        else if(choice == 1)
+        {
+            player.equipment[0] = weaponItem[0].name;
+            for (int i = 0; i < player.list.Count; i++)
+            {
+                if (weaponItem[0].name == player.list[i].name)
+                {
+                    player.list[i].isEquip = true;
+                    player.list[i].name = "[E]" + player.list[i].name;
+                }
+                else if(player.list[i].isEquip == true && player.list[i].type == "[무기]")
+                {
+                    player.list[i].name =  player.list[i].name;
+                }
+            }
+            Equipment(player);
+        }
+        else if (choice == 2 && player.list[1].type == "[무기]")
+        {
+            player.equipment[0] = player.list[1].name;
+            player.list[1].name = "[E]" + player.list[1].name;
+            Equipment(player);
+        }
+
+        else
+        {
+            Console.WriteLine("잘못된 입력입니다.");
+            Thread.Sleep(1000);
+            Equipment(player);
+        }
+    }
+    else if(type == 2)
+    {
+
+    }
+    else
+    {
+
     }
 }
 
@@ -138,6 +213,8 @@ void Shop(Player player)
     "7.[장신구] 스파르타의 방패 | 방어력 +10| 스파르타의 전사들이 사용했다는 전설의 방패입니다. | 3000G",
     "8.[장신구] 팔목보호대 | 방어력 +3 | 착용자의 팔목을 보호해주는 보호대 | 1000G",
 };
+
+
 
     Console.Clear();
     Console.WriteLine("[보유 골드]");
@@ -171,16 +248,13 @@ void Buy(Player player)
     Console.WriteLine("");
     Console.WriteLine("구매하려는 아이탬의 번호를 입력 해주세요.");
     int choice = int.Parse(Console.ReadLine());
-    if (choice == 0)
-    {
-        Shop(player);
-    }
+    if (choice == 0)Shop(player);
     else if (choice == 1)
     {
         if(player.GetGold() >= 600)
         {
             player.SetGold(-600);
-            player.AddItem("낡은 검");
+            player.AddItem(new Item() {type = "[무기]", name ="낡은 검", power=2, price=600, info= "쉽게 볼 수 있는 낡은 검 입니다." });
             Console.WriteLine("구매를 완료했습니다.");
             Thread.Sleep(1000);
             Shop(player);
@@ -197,7 +271,7 @@ void Buy(Player player)
         if (player.GetGold() >= 1500)
         {
             player.SetGold(-1500);
-            player.AddItem("청동 도끼");
+            player.AddItem(new Item() { type = "[무기]", name = "청동 도끼", power = 5, price = 1500, info = "무쇠로 만들어져 튼튼한 갑옷입니다." });
             Console.WriteLine("구매를 완료했습니다.");
             Thread.Sleep(1000);
             Shop(player);
@@ -214,7 +288,7 @@ void Buy(Player player)
         if (player.GetGold() >= 3500)
         {
             player.SetGold(-3500);
-            player.AddItem("스파르타의 창");
+            player.AddItem(new Item() { type = "[무기]", name = "스파르타의 창", power = 9, price = 3500, info = "스파르타의 전사들이 사용했다는 전설의 창입니다." });
             Console.WriteLine("구매를 완료했습니다.");
             Thread.Sleep(1000);
             Shop(player);
@@ -234,7 +308,7 @@ void Buy(Player player)
         if (player.GetGold() >= 1000)
         {
             player.SetGold(-1000);
-            player.AddItem("수련자의 갑옷");
+            player.AddItem(new Item() { type = "[방어구]", name = "수련자의 갑옷", power = 5, price = 1000, info = "수련에 도움을 주는 갑옵 입니다." });
             Console.WriteLine("구매를 완료했습니다.");
             Thread.Sleep(1000);
             Shop(player);
@@ -252,7 +326,7 @@ void Buy(Player player)
         if (player.GetGold() >= 2000)
         {
             player.SetGold(-2000);
-            player.AddItem("무쇠갑옷 ");
+            player.AddItem(new Item() { type = "[방어구]", name = "무쇠갑옷 ", power = 9, price = 2000, info = "무쇠로 만들어져 튼튼한 갑옷입니다." });
             Console.WriteLine("구매를 완료했습니다.");
             Thread.Sleep(1000);
             Shop(player);
@@ -270,7 +344,7 @@ void Buy(Player player)
         if (player.GetGold() >= 3500)
         {
             player.SetGold(-3500);
-            player.AddItem("스파르타의 갑옷");
+            player.AddItem(new Item() { type = "[방어구]", name = "스파르타의 갑옷", power = 15, price = 3500, info = "스파르타의 전사들이 사용했다는 전설의 갑옷입니다." });
             Console.WriteLine("구매를 완료했습니다.");
             Thread.Sleep(1000);
             Shop(player);
@@ -288,7 +362,7 @@ void Buy(Player player)
         if (player.GetGold() >= 3000)
         {
             player.SetGold(-3000);
-            player.AddItem("스파르타의 방패");
+            player.AddItem(new Item() { type = "[장신구]", name = "스파르타의 방패", power = 10, price = 3000, info = "스파르타의 전사들이 사용했다는 전설의 방패입니다." });
             Console.WriteLine("구매를 완료했습니다.");
             Thread.Sleep(1000);
             Shop(player);
@@ -306,7 +380,7 @@ void Buy(Player player)
         if (player.GetGold() >= 1000)
         {
             player.SetGold(-1000);
-            player.AddItem("팔목보호대");
+            player.AddItem(new Item() { type = "[장신구]", name = "팔목보호대", power = 3, price = 1000, info = "착용자의 팔목을 보호해주는 보호대입니다." });
             Console.WriteLine("구매를 완료했습니다.");
             Thread.Sleep(1000);
             Shop(player);
@@ -409,11 +483,13 @@ class Player
     private int atk = 10;
     private int def = 5;
     private int gold = 10000;
+    public string[] equipment = {"","",""};    //차례 대로 무기, 방어구, 장신구
 
-    private List<string> list = new List<string>();
+
+    public List<Item> list = new List<Item>();
     private int maxInventory = 20;
 
-    public void AddItem(string ItemName)
+    public void AddItem(Item ItemName)
     {
         if(list.Count <= maxInventory)
         {
@@ -425,87 +501,42 @@ class Player
         }
     }
 
-    public void GetItem()
+    public void GetItemName()
     {
-        for(int i = 0; i < list.Count; i++)
+        if(list.Count == 0)
         {
-            Console.WriteLine(list.ElementAt(i));
+            Console.WriteLine("보유중인 아이템이 없습니다.");
+        }
+        else{
+            for (int i = 0; i < list.Count; i++)
+            {
+                Console.WriteLine(list.ElementAt(i).name);
+            }
         }
     }
 
-    public int SetMaxHp(int add)
-    {
-        maxHp += add;
-        return maxHp;
-    }
+    public int SetMaxHp(int add){maxHp += add; return maxHp;}
+    public int GetMaxHp(){return maxHp;}
+    public int SetHp(int add){currentHp += add; return currentHp;}
+    public int GetHp(){return currentHp;}
+    public int SetAtk(int add){atk += add; return atk;}
+    public int GetAtk(){return atk;}
+    public int SetDef(int add){def += add; return def;}
+    public int GetDef(){return def;}
+    public string SetClassName(string jobname){className = jobname; return className;}
+    public string GetClassName(){return className;}
+    public int SetLevel(){return level++;}
+    public int GetLevel(){return level;}
+    public int GetGold(){return gold;}
+    public int SetGold(int addGold){gold += addGold; return gold;}
+}
 
-    public int GetMaxHp()
-    {
-        return maxHp;
-    }
-
-    public int SetHp(int add)
-    {
-        currentHp += add;
-        return currentHp;
-    }
-
-    public int GetHp()
-    {
-        return currentHp;
-    }
-
-    public int SetAtk(int add)
-    {
-        atk += add;
-        return atk;
-    }
-
-    public int GetAtk()
-    {
-        return atk;
-    }
-
-    public int SetDef(int add)
-    {
-        def += add;
-        return def;
-    }
-
-    public int GetDef()
-    {
-        return def;
-    }
-
-    public string SetClassName(string jobname)
-    {
-        className = jobname;
-        return className;
-    }
-    public string GetClassName()
-    {
-        return className;
-    }
-
-
-    public int SetLevel()
-    {
-        return level++;
-    }
-
-    public int GetLevel()
-    {
-        return level;
-    }
-
-    public int GetGold()
-    {
-        return gold;
-    }
-
-    public int SetGold(int addGold)
-    {
-        gold += addGold;
-        return gold;
-    }
+class Item
+{
+    public string type;
+    public string name;
+    public int power;
+    public int price;
+    public string info;
+    public bool isEquip = false;
 }
--- ConsoleText/obj/Debug/net8.0/ConsoleText.AssemblyInfo.cs
+++ ConsoleText/obj/Debug/net8.0/ConsoleText.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleText")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+16bf130a79c1cdd9a391db1866d0577353aa9959")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8efe788e419e6df0a2d0cb1f62c4603bf0581405")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleText")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleText")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
